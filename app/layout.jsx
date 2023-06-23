df286c7bf3cde83d991af5646094a35faefa6c14
jaysoncano <cano.jayson10@gmail.com>
2023-06-23 14:16:51 +0800

    convert reactjs to nextjs project files

---
+++ app/layout.jsx
@@ -0,0 +1,22 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './globals.css';
+import Navigation from '../components/navigation';
+import Footer from '../components/footer';
+
+
+export const metadata = {
+  title: 'Welcome to devfund.net',
+  description: 'devfund.net - Join our exclusive community of like minded people on devfund.net',
+}
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body>
+        <Navigation />
+        {children}
+        <Footer />
+      </body>
+    </html>
+  )
+}
