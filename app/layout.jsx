ebac1fe1bdace7a379c23ea85a74d4e0102097e2
jaysoncano <cano.jayson10@gmail.com>
2023-06-26 11:00:58 +0800

    update

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,9 +1,5 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './globals.css';
-import Navigation from '../components/navigation';
-import Footer from '../components/footer';
-
-
 export const metadata = {
   title: 'Welcome to devfund.net',
   description: 'devfund.net - Join our exclusive community of like minded people on devfund.net',
@@ -13,13 +9,9 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
-
- 
       </head>
       <body>
-        <Navigation />
         {children}
-        <Footer />
       </body>
     </html>
   )
