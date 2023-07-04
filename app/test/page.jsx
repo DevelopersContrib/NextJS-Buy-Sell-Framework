d6dd77467f50a88c4801818a65a2882343256fca
rona <row.none@gmail.com>
2023-07-04 14:18:20 +0800

    ai component

--- app/test/page.jsx
+++ app/test/page.jsx
@@ -1,13 +1,42 @@
 import Script from 'next/script';
-import { getDomain } from '../../lib/data';
+import { getDomain,getHost,getScript } from '../../lib/data';
 
 const page = async () => {
 	const domain = getDomain();
+	const VERCEL_URL = process.env.VERCEL_URL;
+	const NEXT_PUBLIC_VERCEL_URL = process.env.NEXT_PUBLIC_VERCEL_URL;
+	const dictionary = Object.entries(process.env);
+	//const host = getHost();
+	//const referer = await getScript('https://tools.contrib.com/site/gethost');
+	//console.log('referer',referer)
 	return (
 		<>
 			<div >test page</div>
+			{/*<p>referer: {referer}</p>
+			<p>host: {host}</p>*/}
+			<p>{VERCEL_URL}</p>
+			<p>{NEXT_PUBLIC_VERCEL_URL}</p>
+			{/*
 			<Script src={"https://tools.contrib.com/widget/fheader?d="+domain+"&container=fheader-script"}/>
       <div className="fheader-script"></div>
+			*/}
+
+<			table>
+        <thead>
+          <tr>
+            <th>VAR</th>
+            <th>Value</th>
+          </tr>
+        </thead>
+        <tbody>
+          {dictionary.map(([key, value]) => (
+            <tr key={key}>
+              <td>{key}</td>
+              <td>{value}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
 		</>
   )
 }
--- lib/data.jsx
+++ lib/data.jsx
@@ -1,7 +1,28 @@
 import axios from 'axios'
 
+export function getHost() {
+  var os = require("os");
+  //console.log('os',os)
+  var hostname = os.hostname();
+
+  return hostname;
+}
+
+export async function getHost2() {
+  const domain = process.env.NEXT_PUBLIC_VERCEL_URL
+  const url = `https://tools.contrib.com/site/gethost`
+  const res = await fetch(url)
+  console.log('res',res)
+  if (!res.ok) {
+    throw new Error('Failed to fetch data')
+  }
+  
+  return res.json()
+}
+
 export function getDomain() {
-  return process.env.NEXT_PUBLIC_VERCEL_URL==='localhost:3000'?'javapoint.com':process.env.NEXT_PUBLIC_VERCEL_URL;
+  //return process.env.NEXT_PUBLIC_VERCEL_URL==='localhost:3000'?'javapoint.com':process.env.NEXT_PUBLIC_VERCEL_URL;
+  return process.env.VERCEL_URL || process.env.NEXT_PUBLIC_VERCEL_URL;
 }
 
 export async function getData() {
