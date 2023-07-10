43359c8f5ea7a658bf52735b88ed0c25119ce1b5
ron <row.none@gmail.com>
2023-07-10 10:52:57 +0800

    set default title if null

--- app/layout.jsx
+++ app/layout.jsx
@@ -6,7 +6,7 @@ import Script from 'next/script';
 export async function generateMetadata({ params, searchParams }, parent) {
   const c = await getData();
 	return {
-		title: c.data.title===''?'Welcome to '+c.data.domainName:c.data.title,
+		title: c.data.title ? c.data.title : 'Welcome to '+c.data.domainName,
 		description: c.data.description,
     keywords: c.data.keywords,
     author: c.data.author
