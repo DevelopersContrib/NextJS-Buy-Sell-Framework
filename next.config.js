bb1f0588fb922f6ee6c0f93a0ae2611cf437ac00
Chikaysci <kjabellar@gmail.com>
2024-10-08 13:27:40 +0800

    updated vbot blogs

--- next.config.js
+++ next.config.js
@@ -33,7 +33,7 @@ const nextConfig = {
     ];
   },
   images: {
-    domains: ['cdn.vnoc.com', 'vnoclogos.s3-us-west-1.amazonaws.com', 'tools.contrib.com','projectcafe.com', 'contrib.com','vnoclogos.s3-us-west-1.amazonaws.com','vnoclogos.s3.amazonaws.com'],
+    domains: ['cdn.vnoc.com', 'vnoclogos.s3-us-west-1.amazonaws.com', 'tools.contrib.com','projectcafe.com', 'contrib.com','vnoclogos.s3-us-west-1.amazonaws.com','vnoclogos.s3.amazonaws.com','vbot-images.s3.us-east-1.amazonaws.com'],
   },
 }
 
